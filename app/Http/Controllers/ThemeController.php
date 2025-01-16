be792f5e2f6ee0e3852ea4ebeb76cc9703f53822
Dmitry! <ibaron1337@yandex.ru>
2025-01-16 18:32:48 +0300

    created Theme/Show page

--- app/Http/Controllers/ThemeController.php
+++ app/Http/Controllers/ThemeController.php
@@ -41,7 +41,9 @@ class ThemeController extends Controller
      */
     public function show(Theme $theme)
     {
-        //
+        $theme = ThemeResource::make($theme)->resolve();
+
+        return inertia('Theme/Show', compact('theme'));
     }
 
     /**
