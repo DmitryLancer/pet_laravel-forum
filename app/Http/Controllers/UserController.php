221b0c99d7696989f018241a080e41478d5127c3
Dmitry! <ibaron1337@yandex.ru>
2025-01-18 22:47:33 +0300

    crop avatar and download plugin -         "intervention/image": "^3.11",

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\User\UpdateRequest;
 use App\Http\Resources\User\UserResource;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManager;
+
 
 class UserController extends Controller
 {
@@ -21,9 +22,17 @@ class UserController extends Controller
 
         $path = Storage::disk('public')->put('/avatars', $data['avatar']);
 
+        if (auth()->user()->avatar) {
+            Storage::disk('public')->delete(auth()->user()->avatar);
+        }
+
         auth()->user()->update([
             'avatar' => $path
         ]);
+
+        $path = ImageManager::imagick()->read('storage/' . $path);
+        $path->resize(100, 100);
+        $path->save();
 
         return UserResource::make(auth()->user())->resolve();
     }
--- resources/js/Pages/User/Personal.vue
+++ resources/js/Pages/User/Personal.vue
@@ -14,8 +14,8 @@
         <p class="mb-2">
           Change avatar
         </p>
-        <a href="#" @click.prevent="this.$refs.avatar_load.click()" class="block w-24 h-24 rounded-full bg-gray-300">
-          <img v-if="user.avatar_url" :src="user.avatar_url" alt="{{ user.name }}">
+        <a href="#" @click.prevent="this.$refs.avatar_load.click()" class="block w-24 h-24 rounded-full overflow-hidden bg-gray-300">
+          <img v-if="user.avatar_url" class="w-full w-24 h-24 block" :src="user.avatar_url" alt="{{ user.name }}">
         </a>
         <div hidden>
           <input @change="storeAvatar" ref="avatar_load" type="file">
